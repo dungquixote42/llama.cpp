d83bca6a7af65fa38932403189ddc77355c623fe
banana split <hyunsoo.david.kim@gmail.com>
2026-02-09 19:24:18 -0500

    skt/a.x-k1: add model to convert_hf_to_gguf_update.py

--- convert_hf_to_gguf_update.py
+++ convert_hf_to_gguf_update.py
@@ -148,6 +148,7 @@ models = [
     {"name": "youtu",            "tokt": TOKENIZER_TYPE.BPE, "repo": "https://huggingface.co/tencent/Youtu-LLM-2B", },
     {"name": "solar-open",       "tokt": TOKENIZER_TYPE.BPE, "repo": "https://huggingface.co/upstage/Solar-Open-100B", },
     {"name": "exaone-moe",       "tokt": TOKENIZER_TYPE.BPE, "repo": "https://huggingface.co/LGAI-EXAONE/K-EXAONE-236B-A23B", },
+    {"name": "axk1",             "tokt": TOKENIZER_TYPE.BPE, "repo": "https://huggingface.co/skt/A.X-K1", },
 ]
 
 # some models are known to be broken upstream, so we will skip them as exceptions
